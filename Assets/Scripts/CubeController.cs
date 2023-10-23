f06a62635561891719180e5277cb4a2ecece2753
rozkhara <nytemute@gmail.com>
2023-10-24 00:09:04 +0900

    Initial Commit
    Unity 2022.3.9f1

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    [SerializeField] 
+    private List<GameObject> prefabs;
+
+    public void ClearCube()
+    {
+        // set active false
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                DontDestroyOnLoad(_instance);
+            }
+            return _instance;
+        }
+    }
+
+    public bool isPaused { get; private set; }
+    public int Difficulty { get; private set; } // 1 for Easy, 2 for Medium, 3 for Hard, 0 for every other scenes
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            if (_instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            return;
+        }
+        _instance = GetComponent<GameManager>();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0; isPaused = true;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1; isPaused = false;
+    }
+    public void SetDifficulty(int _difficulty)
+    {
+        Difficulty = _difficulty;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
